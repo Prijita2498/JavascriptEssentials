81378dff82ee594943e71c5dff387112b9fdc24c
Prijita2498 <drastimedia98@gmail.com>
2025-07-03 18:02:22 -0400

    Employee Specialization Task Added

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java' },
     //... More employee records can be added here
   ];
 
@@ -39,6 +39,20 @@ function findEmployeeById(employeeId){
     }
     else{
         document.getElementById('employeeDetails').innerHTML = 'No Employee Found with this ID';
+    }
+}
+
+// Find Employee by specialization
+function findEmployeeBySpecialization(employeeSpecialization){
+    const foundEmployee = employees.find(employee => employee.specialization === employeeSpecialization);
+    console.log(foundEmployee);
+    if(foundEmployee){
+        document.getElementById('employeeDetails').innerHTML = `
+        <p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>  
+      `;
+    }
+    else{
+        document.getElementById('employeeDetails').innerHTML = 'No Employees Found With this Specialization';
     }
 }
 
